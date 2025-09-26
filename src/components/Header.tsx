da6ed37fa4337a08d985eed6f3d2434a2487e304
Petros <petrosluros@gmail.com>
2025-09-26 19:36:24 +0300

    feat : add layout with HEader and footer

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+
+
+const Header = () => {
+    return (
+        <>
+        <header className="bg-cf-dark-red w-full">
+            <div className="container mx-auto px-5 flex items-center justify-between">
+                <img className="my-4 h-16" src="https://codingfactory.aueb.gr/sites/all/themes/cf_theme/logo.png" alt=""/>
+                <nav className="flex gap-4 text-white font-medium"><a href="#">Home</a></nav>
+            </div>
+        </header>
+        </>
+    )
+}
+
+export default Header;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,25 @@
+import Header from "./Header.tsx";
+import Footer from "./Footer.tsx";
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+
+const Layout = ({children} : LayoutProps) => {
+
+    return (
+        <>
+            <Header />
+            <div className="container mx-auto-pt-24 min-h-95vh">
+
+                {children}
+            </div>
+
+
+        <Footer />
+        </>
+    )
+}
+
+export default Layout;
