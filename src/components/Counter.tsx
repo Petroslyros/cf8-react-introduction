c048642887189042f9ee5c9be9948d1e31bc1154
Petros <petrosluros@gmail.com>
2025-10-02 18:43:32 +0300

    feat: added counter button for the counter tsx

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import CounterButton from "./CounterButton.tsx";
 
 
 const Counter = () => {
@@ -25,15 +26,9 @@ const Counter = () => {
             <>
                 <h1 className="text-center my-12">Count is <strong>{count}</strong></h1>
                 <div className="text-center space-x-4">
-                    <button onClick={increaseCount} className="bg-cf-dark-gray text-white py-2 px-4">
-                        Increase
-                    </button>
-                    <button disabled={count===0} onClick={decreaseCount} className="bg-cf-dark-gray disabled:bg-cf-gray text-white py-2 px-4">
-                        Decrease
-                    </button>
-                    <button onClick={resetCount} disabled={count===0} className="bg-cf-dark-gray disabled:bg-cf-gray text-white py-2 px-4">
-                        Reset to 0
-                    </button>
+                    <CounterButton onClick={increaseCount} label="Increase" />
+                    <CounterButton onClick={decreaseCount} label="Decrease" disabled={count === 0}/>
+                    <CounterButton onClick={resetCount} label="Reset" disabled={count === 0} addClasses="bg-cf-dark-red disabled:bg-red-500"/>
                 </div>
             </>
         </>
