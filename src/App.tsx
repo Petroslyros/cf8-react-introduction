4052de6fb553eb246597d9f3ac984ac4d46384ba
Petros <petrosluros@gmail.com>
2025-10-02 20:48:46 +0300

    feat: add counter advanced, counter with custom hook, counter with more states, name change with effect and useCounter hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
-
 // import ViteIntro from "./components/ViteIntro/ViteIntro.tsx"
-
 // import ClassComponent from "./components/ClassComponent.tsx";
 // import FunctionalComponent from "./components/FunctionalComponent.tsx";
 // import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
 // import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
 // import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
 // import Card from "./components/Card.tsx";
-
 import Layout from "./components/Layout.tsx";
+// import CounterWithCustomHook from "./components/CounterWithCustomHook.tsx";
+import NameChangerWithEffect from "./components/NameChangerWithEffect.tsx";
+// import {useEffect} from "react";
 // import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
 // import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
 // import Counter from "./components/Counter.tsx";
-import NameChanger from "./components/NameChanger.tsx";
+// import NameChanger from "./components/NameChanger.tsx";
+// import CounterWithMoreStates from "./components/CounterWithMoreStates.tsx";
+// import CounterAdvanced from "./components/CounterAdvanced.tsx";
 
 function App() {
 
+    // useEffect(setup, []);
+    //
+    // useEffect(() => {
+    //     //setup function
+    //     return () => {
+    //         //clean up
+    //     }
+    //
+    // });
 
   return (
     <>
@@ -34,8 +45,13 @@ function App() {
            {/*<ArrowFunctionalComponentWithPropsType title="Title" description="Description"/>*/}
            {/*<FunctionalComponentWithState />*/}
            {/*<Counter />*/}
-           <NameChanger/>
+           {/*<NameChanger/>*/}
+           {/* <CounterWithMoreStates/>*/}
+           {/* <CounterAdvanced/>*/}
+           {/* <CounterWithCustomHook/>*/}
+            <NameChangerWithEffect/>
         </Layout>
+
 
 
     </>
